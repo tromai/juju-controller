a61623574ef328cf2930a13dfbadecf47fe87e5e
Trong Nhan Mai <trongnhan.mai@canonical.com>
2026-08-29 00:27:41 +1000

    hehe

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -5,4 +5,7 @@ import jubilant
 
 def test_controller_charm_is_active():
     controller = jubilant.Juju(model="controller")
-    controller.wait(lambda status: jubilant.all_active(status, 'controller'), timeout=10,)
+    controller.wait(
+        lambda status: jubilant.all_active(status, 'controller'),
+        timeout=10,
+    )
